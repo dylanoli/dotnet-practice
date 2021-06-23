f6ab6ab8cfc7e22246d5dc91f983c5e68c519a78
Dylan <dylan.oli@hotmail.com>
2021-06-23 13:45:55 -0300

    refact: update libs

--- api/Models/Person.cs
+++ api/Models/Person.cs
@@ -1,5 +1,7 @@
+using System.ComponentModel.DataAnnotations.Schema;
 namespace api.Models
 {
+    [Table("person")]
     public class Person
     {
         public struct SGender
@@ -7,10 +9,13 @@ namespace api.Models
             public const string MALE = "male";
             public const string FEMALE = "female";
         }
-
+        [Column("id")]
         public long Id { get; set; }
+        [Column("name")]
         public string Name { get; set; }
+        [Column("age")]
         public int Age { get; set; }
+        [Column("gender")]
         public string Gender { get; set; }
     }
 }
